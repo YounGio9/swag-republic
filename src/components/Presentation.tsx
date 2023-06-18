ec7e4fa7824cb5201bbe6218842c3e0bc02b50d4
Giovani <desouzagiovani9@gmail.com>
2023-06-18 19:00:52 +0100

    Update footer links

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -40,7 +40,7 @@ function Presentation() {
                         color='white'
                         handleClick={() => router.push("/shop")}
                      >
-                        <span className=' mb-14'>Open the market</span>
+                        <span className=' mb-14'>Open the store</span>
                      </CustomButtom>
                   </a>
                </div>
